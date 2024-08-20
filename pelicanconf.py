0b54caa5b233135d674be98d2bf4030d65b76500
odysseyprogramme <71067068+odysseyprogramme@users.noreply.github.com>
2024-08-20 16:43:17 +0800

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -61,7 +61,7 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 
-THEME = 'theme/'
+THEME = 'themes/bricks/'
 
 STATIC_PATHS = [
     'images',
